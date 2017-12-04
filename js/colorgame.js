5486f46d4978b04be061cd31e3f7c0138f518674
Mkrtich13 <hovanisianm@gmail.com>
2017-12-04 10:20:25 +0100

    ajout des fichiers et création formulaire

---
+++ js/colorgame.js
@@ -0,0 +1,9 @@
+function userSquareNb() {
+  var squareNumber = document.getElementById("settingsSquare__Nb").value;
+  console.log(squareNumber);
+}
+
+function userTimer() {
+  var timer = document.getElementById("settingsTimer__Nb").value;
+  console.log(timer);
+}
